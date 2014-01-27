3f20b89f1c6bc458b10dcc40fa70ceb0cbc437b5
cwilli13 <ryanwilliams92@gmail.com>
2014-01-27 13:19:15 -0500

    Updated Player Class
    Added string name, int wins, int losses and according functions to
    Player class.  User inputs name, wins and losses set to 0.

--- Player.java
+++ Player.java
@@ -1,8 +1,64 @@
+/* C. Ryan Williams 1/27/14
+	
+Set Player class. Will include;
+	- string name
+	- int wins
+	- int losses
+	 */
+
+import java.util.Scanner;
+/*import java.util.*; */
+
 public class Player{
 
-	public Player(){
-		System.out.println("player created");
+	private String name;
+	private int wins = 0;
+	private int losses = 0;
 
+	Scanner userInput = new Scanner(System.in);
+
+	public Player(){
+		System.out.println("Creating Player...");
 		
+		/* Get user's name */
+		setName();
+
+		System.out.println("\n\n\nWelcome to Mastermind " + name + ".");
+		System.out.println("\nYour record:");
+		System.out.println("Wins - " + wins);
+		System.out.println("Losses - " + losses + "\n");
+
 	}
+
+	public String setName(){
+		System.out.println("Enter Your Name:");
+		name = userInput.nextLine();
+
+		return name;
+	}
+
+	public String getName(){
+
+		return name;
+	}
+
+	public int setWins(){
+		wins++;
+
+		return wins;
+	}
+
+	public int getWins(){
+		return wins;
+	}
+
+	public int setLosses(){
+		losses++;
+
+		return losses;
+	}
+	public int getLosses(){
+		return losses;
+	}
+
 }
